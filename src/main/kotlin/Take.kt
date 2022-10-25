31eb4c3254d311fedb38e0c1b48965da3096e76b
tmdgusya <tmdgusya@naver.com>
2022-10-25 23:28:00 +0900

    Take

---
+++ src/main/kotlin/Take.kt
@@ -0,0 +1,15 @@
+
+fun <T> Iterator<T>.take(limit: Int): List<T> {
+    val result = mutableListOf<T>()
+    for (ele in this) {
+        if (result.size == limit) return result
+        result.add(ele)
+    }
+    lazy {  }
+    return result
+}
+
+fun main() {
+    println(listOf(1, 2, 3, 4, 5, 6).iterator().take(4))
+    sequenceOf(1, 2, 3, 4, 5, 6).take(4)
+}
